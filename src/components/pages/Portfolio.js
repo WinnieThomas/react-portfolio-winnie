506bb285840b9d3ab604d5be3986cf45607ac6d4
WinnieThomas <winnieantony1988@gmail.com>
2023-04-10 17:31:14 +0100

    added styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -16,40 +16,40 @@ const data = [
     image:IMG1,
     title:'Whats On Where',
     desc:'An app which shows the events based on the user input',
-    github:'',
-    link:'',
+    github:'https://github.com/MindOfDevelopment/Whats-On-Where',
+    link:'https://mindofdevelopment.github.io/Whats-On-Where/',
   },
   {
     id:2,
     image:IMG2,
     title:'Weather API',
     desc:'An app which shows the weather for the next 5days.',
-    github:'',
-    link:'',
+    github:'https://github.com/WinnieThomas/WeatherAPI',
+    link:'https://winniethomas.github.io/WeatherAPI/',
   },
   {
     id:3,
     image:IMG3,
     title:'Book Search Engine',
     desc:'An app where the user can search books and also save them for future',
-    github:'',
-    link:'',
+    github:'https://github.com/WinnieThomas/Book-search-engine',
+    link:'https://boiling-oasis-89765.herokuapp.com/',
   },
   {
     id:4,
     image:IMG4,
-    title:'Note Taker',
-    desc:'An app which allows user to enter the notes allowing the users to better organize their thoughts ',
-    github:'',
-    link:'',
+    title:'Workday Scheduler',
+    desc:'An app which allows user to enter the schedule allowing the users to better manage their time ',
+    github:'https://github.com/WinnieThomas/Workday-Scheduler',
+    link:'https://winniethomas.github.io/Workday-Scheduler/',
   },
   {
     id:5,
     image:IMG5,
     title:'PWA Text Editor',
     desc:'First Progressive Web application development',
-    github:'',
-    link:'',
+    github:'https://github.com/WinnieThomas/PWA-TextEditor',
+    link:'https://pure-lake-47302.herokuapp.com/',
   }
 
 ]
@@ -66,8 +66,8 @@ export default function Portfolio() {
            <div className='portfolioimg-container'>
             <img src={image} alt={title} />
            </div>
-           <h3>title</h3>
-           <h4>desc</h4>
+           <h3>{title}</h3>
+           <p>{desc}</p>
            <div className="portfolioitem-cta">
                   <a href={github} className='btn' target='_blank' rel="noopener noreferrer" ><FaGithub />Github</a>
                   <a href={link} className='btn btn-primary' target='_blank' rel="noopener noreferrer"><GrDeploy />Deployed Link</a>
