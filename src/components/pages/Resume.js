252a3067fe5f21a1efa044bc9e01e150607cdb2c
WinnieThomas <winnieantony1988@gmail.com>
2023-04-11 20:37:04 +0100

    modified resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import CV from '../../assets/winniethomas.pdf';
 
 export default function Resume() {
   return (
     <div>
       <h3 className="mb-2 pb-2">
                   <strong>Resume:
-                <a target="_blank" rel="noopener noreferrer" className="tab4" href={process.env.PUBLIC_URL + "/assets/winnie.pdf"}>  My resume</a></strong>
+                <a target="_blank" rel="noopener noreferrer" className="tab4" href={CV}> Click Here</a></strong>
                 </h3>
       <div className='skill'>
         <h3>Proficiencies</h3>
